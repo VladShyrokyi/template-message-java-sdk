2bd259edf8dd9c3e97aaf6175f6922f7faad47f6
Shyrokyi Vladislav <vlad16062001@gmail.com>
2021-07-24 14:49:16 +0300

    [ADD] Default dynamic variable name

--- src/main/java/template_message_sdk/DefaultRegex.java
+++ src/main/java/template_message_sdk/DefaultRegex.java
@@ -2,6 +2,7 @@ package template_message_sdk;
 
 public final class DefaultRegex {
     public static final String REGEX = "%\\[([^%,\\s]+)\\]%";
+    public static final String DYNAMIC_VARIABLE_NAME = "DYN_VAR";
 
     private DefaultRegex() {}
 
